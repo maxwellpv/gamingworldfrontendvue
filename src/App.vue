d7394de2f4bceb3ec10aeb459421b50514f04dc3
Aaron <aaron.plan@hotmail.com>
2021-09-25 19:01:52 -0500

    First Version: Nav bar, News Page and filters of news.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,161 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <!--  Barra de navegacion  -->
+    <v-container>
+      <v-app-bar app color="primary" flat dark>
+
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>GamingWorld</v-toolbar-title>
+
+        <v-text-field class="d-flex mt-auto " style="max-width: 250px" label="Search ..." append-icon="mdi-magnify"  solo  background-color="primary" dark></v-text-field>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+
+
+        <v-menu left bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+                icon
+                v-bind="attrs"
+                v-on="on"
+            >
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item
+                v-for="n in 5"
+                :key="n"
+                @click="() => {}"
+            >
+              <v-list-item-title>Option {{ n }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+
+
+
+
+      </v-app-bar>
+    </v-container>
+
+
+    <v-main>
+
+
+      <v-div >
+        <v-row no-gutters>
+          <v-col cols="12" sm="2">
+            <v-card>
+              <v-list>
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-btn @click="searchArticles('videogames')">VideoGames</v-btn>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-btn @click="searchArticles('steam')">Steam</v-btn>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-btn @click="searchArticles('twitch')">Twitch</v-btn>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
+
+          </v-col>
+
+          <v-col cols="12" sm="8">
+            <v-container fluid>
+              <main-content :articles="articles"></main-content>
+            </v-container>
+          </v-col>
+
+          <v-col cols="12" sm="2">
+            <v-card>
+              equisde
+            </v-card>
+
+          </v-col>
+        </v-row>
+
+
+
+      </v-div>
+
+
+
+      <v-footer class="secondary" app>
+        <v-layout row wrap align-center>
+          <v-flex xs12>
+            <div class="white--text ml-3"></div>
+            Made whit
+            <v-icon>
+
+            </v-icon>
+
+          </v-flex>
+        </v-layout>
+
+      </v-footer>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from "axios";
+import MainContent from "@/components/main-content";
+
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    MainContent
+
+  },
+
+  data: () => ({
+    drawer: false,
+    apiKey: '30a01aa6438d4782906f35bb2f136a91',
+    articles:[],
+    errors:[]
+  }),
+
+  created() {
+    axios.get(`https://newsapi.org/v2/everything?q=Gamer&language=es&apiKey=${this.apiKey}`)
+        .then(response => {
+          this.articles = response.data.articles;
+          console.log('data: ');
+          console.log(response.data.articles)
+        })
+  },
+
+  methods:{
+    searchArticles(theme){
+      this.drawer = !this.drawer;
+      axios.get(`https://newsapi.org/v2/everything?q=${theme}&apiKey=${this.apiKey}`)
+          .then(response => {
+            this.articles = response.data.articles;
+            console.log('data: ');
+            console.log(response.data.articles);
+          })
+          .catch(e=>{
+            this.errors.push(e);
+          })
+    }
+  }
+
+};
+</script>
---
+++ src/components/main-content.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-layout row wrap align-center filled>
+    <v-flex xs8 offset-md2>
+
+
+        <v-row >
+
+          <template v-for="(article, index) in articles">
+
+            <v-col :cols="index === 0 ? 12 : 6" :key="article.title">
+              <v-card class="my-3" hover>
+                <v-img height="350px" v-bind:src="article.urlToImage"></v-img>
+                <v-container fill-height fluid>
+                  <v-layout>
+                    <v-flex xs12 align-end d-flex>
+                      <span class="headline">{{article.title}}</span>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+                <v-card-text>
+                  {{article.description}}
+                </v-card-text>
+                <v-card-actions>
+                  <v-chip small color="secondary" class="white--text">
+                    {{article.source.name}}
+                  </v-chip>
+                  <v-spacer></v-spacer>
+                  <v-btn icon class="red--text">
+                    <v-icon small>mdi-reddit</v-icon>
+                  </v-btn>
+                  <v-btn small replace color="info" v-bind:href="article.url" target="_blank">Read More</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+
+          </template>
+
+        </v-row>
+
+
+
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  name: "main-content",
+  props: {
+    articles: Array
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
