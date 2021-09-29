7a3384b520e46351237277ea57b8f8c6c9853b17
Lossani <58919471+Lossani@users.noreply.github.com>
2021-09-28 22:17:11 -0500

    Revert "Feature/newspage"

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,28 @@
 <template>
-  <v-app>
-
-
-      <nav-bar></nav-bar>
-
-      <v-main class="background">
-
-        <!--  ScrollToTop Button  -->
-        <v-container>
-          <v-btn class="mb-5"
-              v-scroll="onScroll"
-              v-show="fab"
-              fab
-              dark
-              fixed
-              bottom
-              right
-              color="primary"
-              @click="toTop"
-          >
-            <v-icon>mdi-chevron-up</v-icon>
-          </v-btn>
-        </v-container>
-        <news-page></news-page>
-      </v-main>
-    <footer-content></footer-content>
-  </v-app>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-
-
-import NavBar from "./components/nav-bar";
-import FooterContent from "./components/footer-content";
-import NewsPage from "./views/news-page";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-
   components: {
-    NavBar,
-    FooterContent,
-    NewsPage
-  },
-
-  data: () => ({
-    fab:false,
-  }),
-
-  methods:{
-
-    onScroll (e) {
-      if (typeof window === 'undefined') return
-      const top = window.pageYOffset ||   e.target.scrollTop || 0
-      this.fab = top > 20
-    },
-    toTop () {
-      this.$vuetify.goTo(0)
-    }
+    HelloWorld
   }
-};
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
