d49633af4d403e1dd9d845f64ac72967af3ddc6a
LoelDev <pmpinzasr@hotmail.com>
2021-10-17 19:13:27 -0500

    Add: Profile added

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import News from '../views/NewsPage.vue'
 import GamerProfile from '../components/UserProfileGamer'
 import StreamerProfile from '../components/UserProfileStreamer'
 import Publications from "@/views/PublicationsPage";
+import EditProfile from "../views/EditProfileGamer";
+import MyProfile from "../views/MyProfile";
 
 
 Vue.use(VueRouter)
@@ -33,7 +35,16 @@ const routes = [
     name: 'publications',
     component: Publications
   },
-
+  {
+    path: '/edit',
+    name: 'edit',
+    component: EditProfile
+  },
+  {
+    path: '/myprofile',
+    name: 'myprofile',
+    component: MyProfile
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/MyProfile.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <nav-bar></nav-bar>
+    <v-card>AAAAAAAAAAAAAA</v-card>
+    <v-btn class="ma-3 primary" @click="showEditProfile">Edit </v-btn>
+  </v-container>
+</template>
+
+<script>
+import NavBar from "../components/NavBar";
+export default {
+  name: "MyProfile",
+  components: {NavBar},
+  methods:{
+    showEditProfile() {
+      this.$router.push('edit')
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
