f6ef468001093f33277e83cadf8ea24f07ae0550
Lossani <dev@xempre.com>
2021-11-24 06:44:28 -0500

    Feature: Implemented session system.

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -8,11 +8,11 @@
    <v-col cols="6">
      <v-card color="primary" elevation="2" class="pa-16 ml-16" max-width="700">
        <v-card-title class="white--text">Gaming World </v-card-title>
-       <v-text-field color="white" label="Username"></v-text-field>
-       <v-text-field color="white" type="password" label="Password"></v-text-field>
+       <v-text-field color="white" label="Username" v-model="username"></v-text-field>
+       <v-text-field color="white" type="password" label="Password" v-model="password"></v-text-field>
        <div class="text-center">
         <div class="my-1">
-           <v-btn color="primary" class="mr-4" >
+           <v-btn @click="login" color="primary" class="mr-4" >
                Login
             </v-btn>
           </div>
@@ -29,12 +29,32 @@
 </template>
 
 <script>
+import UsersService from "@/services/users.service";
+import SessionService from "@/services/session.service";
+
 export default {
   name: "LoginPage",
+  data() {
+    return {
+      username: "",
+      password: ""
+    }
+  },
   methods: {
     showRegisterPage() {
       this.$router.push({path: '/register'})
     },
+    login() {
+      let data = {username: this.username, password: this.password};
+      UsersService.signIn(data).then(
+          (response) => {
+            SessionService.createSession(response.data);
+            this.$router.push({path: '/'})
+          }
+      ).catch(() => {
+        alert("Wrong user or password.");
+      });
+    }
   }
 }
 </script>
--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -3,16 +3,16 @@
   <v-card color="primary" class="mx-auto mt-16" max-width="600" align="center">
     <v-card-title class="white--text">Sign Up</v-card-title>
     <div class="pa-7">
-    <v-text-field color="white" label="Name"></v-text-field>
-    <v-text-field color="white" label="Last Name"></v-text-field>
-    <v-text-field color="white" type="email" label="Email"></v-text-field>
-    <v-text-field color="white" label="Username"></v-text-field>
-    <v-text-field color="white" type="password" label="Password"></v-text-field>
-    <v-text-field color="white" type="password" label="Confirm Password"></v-text-field>
+    <v-text-field color="white" label="Name" v-model="registerData.firstName"></v-text-field>
+    <v-text-field color="white" label="Last Name" v-model="registerData.lastName"></v-text-field>
+    <v-text-field color="white" type="email" label="Email" v-model="registerData.email"></v-text-field>
+    <v-text-field color="white" label="Username" v-model="registerData.username"></v-text-field>
+    <v-text-field color="white" type="password" label="Password" v-model="registerData.password"></v-text-field>
+    <v-text-field color="white" type="password" label="Confirm Password" v-model="confirmPassword"></v-text-field>
     </div>
     <v-row align="center" justify="space-around">
       <div class="my-1">
-        <v-btn color="primary" class="mb-3" >
+        <v-btn @click="register" color="primary" class="mb-3">
           Register
         </v-btn>
       </div>
@@ -27,8 +27,35 @@
 </template>
 
 <script>
+import UsersService from "@/services/users.service";
+
 export default {
-  name: "RegisterPage"
+  name: "RegisterPage",
+  data: () => {
+    return {
+      confirmPassword: "",
+      registerData: {
+        username: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+      }
+    }
+  },
+  methods: {
+    register() {
+      UsersService.signUp(this.registerData).then(
+          () => {
+            this.$router.push({path: '/login'})
+          }
+      ).catch((error) => {
+        if( error.response ){
+          alert(error.response.data.message);
+        }
+      });
+    }
+  }
 }
 </script>
 
